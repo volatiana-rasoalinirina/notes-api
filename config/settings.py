7db5dec9ce293d705e768187f9f6e47295e1453b
Volatiana Rasoalinirina <volatiana.rasoalinirina.dev@gmail.com>
2026-04-03 16:49:32 +0300

    install rest_framework_simplejwt

--- config/settings.py
+++ config/settings.py
@@ -76,6 +76,12 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 REST_FRAMEWORK = {
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
+    ],
+    "DEFAULT_PERMISSION_CLASSES": [
+        "rest_framework.permissions.IsAuthenticated",
+    ]
 }
 
 SPECTACULAR_SETTINGS = {
